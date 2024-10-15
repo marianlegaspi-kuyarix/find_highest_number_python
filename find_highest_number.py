32b47c01f209b817756424c9fcf8102462725643
Marian <ianlegaspi.marian@gmail.com>
2024-10-16 02:23:41 +0800

    assume that the first number is the highest

--- find_highest_number.py
+++ find_highest_number.py
@@ -1,3 +1,4 @@
 #define the method
 def find_the_highest_number(first_num, second_num, third_num, fourth_num, fifth_num):
- 
+    #assume that the first number is the highest number
+    highest_number = first_num
