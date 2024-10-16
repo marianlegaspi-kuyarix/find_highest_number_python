2cda26abcbd8625165100e213f9380d980d5612b
Marian <ianlegaspi.marian@gmail.com>
2024-10-16 20:43:14 +0800

    user is ask to input five numbers

--- find_highest_number.py
+++ find_highest_number.py
@@ -2,7 +2,7 @@
 def find_the_highest_number(first_num, second_num, third_num, fourth_num, fifth_num):
     #assume that the first number is the highest number
     highest_number = first_num
-    #using if statement to determine highest number
+    #use if statement to determine highest number
     if second_num > highest_number:
         highest_number = second_num
     if third_num > highest_number:
@@ -12,3 +12,11 @@ def find_the_highest_number(first_num, second_num, third_num, fourth_num, fifth_
     if fifth_num > highest_number:
         highest_number = fifth_num
     return highest_number
+#ask the user to input 5 numbers
+first_num = float(input("Enter the first number: "))
+second_num = float(input("Enter the second number: "))
+third_num = float(input("Enter the third number: "))
+fourth_num = float(input("Enter the fourth number: "))
+fifth_num = float(input("Enter the fifth number: "))
+
+
