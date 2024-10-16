e22b426345961a8f26bc92e125f92675a903a793
Marian <ianlegaspi.marian@gmail.com>
2024-10-16 20:35:32 +0800

    Used if statements to determine the highest number

--- find_highest_number.py
+++ find_highest_number.py
@@ -2,3 +2,13 @@
 def find_the_highest_number(first_num, second_num, third_num, fourth_num, fifth_num):
     #assume that the first number is the highest number
     highest_number = first_num
+    #using if statement to determine highest number
+    if second_num > highest_number:
+        highest_number = second_num
+    if third_num > highest_number:
+        highest_number = third_num
+    if fourth_num > highest_number:
+        highest_number = fourth_num
+    if fifth_num > highest_number:
+        highest_number = fifth_num
+    return highest_number
